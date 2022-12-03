8b04a506310181faedf1ca32d6b8952571dae006
MitchellAntoine-FS <amitchell3@student.fullsail.edu>
2022-12-02 16:44:58 -0800

    Initial commit

---
+++ MitchellAntoine_AppleWatch/MitchellAntoine_AppleWatch Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MitchellAntoine_AppleWatch Watch App
+//
+//  Created by Antoine Mitchell on 12/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MitchellAntoine_AppleWatch/MitchellAntoine_AppleWatch/MitchellAntoine_AppleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MitchellAntoine_AppleWatchApp.swift
+//  MitchellAntoine_AppleWatch
+//
+//  Created by Antoine Mitchell on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MitchellAntoine_AppleWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
