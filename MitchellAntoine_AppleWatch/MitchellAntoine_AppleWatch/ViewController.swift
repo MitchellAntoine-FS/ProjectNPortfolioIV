240f64156dd73d49760b313bcdfc5739c1c6b3fe
MitchellAntoine-FS <amitchell3@student.fullsail.edu>
2022-12-06 14:03:22 -0800

    Adding images

--- MitchellAntoine_AppleWatch/MitchellAntoine_AppleWatch/ViewController.swift
+++ MitchellAntoine_AppleWatch/MitchellAntoine_AppleWatch/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         
+        addData()
         
         //Watch Connectivity
         if WCSession.isSupported(){
@@ -33,11 +34,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return strains.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AW01", for: indexPath)
+        
+        cell.textLabel?.text = strains[indexPath.row].name
         
         return cell
     }
@@ -63,6 +66,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func sessionDidDeactivate(_ session: WCSession) {
         print("Session Deactivated")
+    }
+    
+    func addData() {
+        strains.append(Strain(name: "Dosidos", effects: "Sleepy, Relaxed, Sedated", image: "dosidos.jpg"))
+        strains.append(Strain(name: "Ice Cream Cake", effects: "Sleepy, Relaed hungry", image: "ice-cream.jpeg"))
+        strains.append(Strain(name: "Green Crack", effects: "Paranoia, Cotton mouth, Creative", image: "greenCrack.jpeg"))
+        strains.append(Strain(name: "Blue Dream", effects: "Relaxed, Cotton Mouth, Creative", image: "blueDream.jpeg"))
+        strains.append(Strain(name: "Hindu Kush", effects: "Relaxed, Sleepy, Cotton Mouth", image: "hinduKush.png"))
+        strains.append(Strain(name: "Gorilla Glue", effects: "Sleepy, Headache, Sedated", image: "gorillaglue.jpeg"))
+        strains.append(Strain(name: "Wedding Cake", effects: "Relaxed, Aroused, Hungry", image: "weddingCake.png"))
+        strains.append(Strain(name: "Skywalker OG", effects: "Relaxed, Sleepy, Sedated", image: "skywalkOG.jpeg"))
+        strains.append(Strain(name: "Northern Lights", effects: "Relaxed, Sleepy, Coton mouth", image: "northernLights.jpeg"))
+        strains.append(Strain(name: "Girl Scout Cookies", effects: "Relaxed, Happy, Creative", image: "girlScoutCookies.jpeg"))
     }
     
 
